a68cbd214b6a8ac81d6d770b47109f71deb69e32
nvak1999 <76765929+nvak1999@users.noreply.github.com>
2021-04-04 16:08:32 +0700

    asdasda

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const API_KEY = "563492ad6f917000010000017e95324e49094d6d84bbce34fa6d31da";
+const API_KEY = "563492ad6f91700001000001c2f2503f84dd4f78bfd1c5e0d6ce9653";
 
 let getImage = async () => {
-  const baseURL = "https://api.pexels.com/v1/curated?page=99&per_page=80";
+  const baseURL = "https://api.pexels.com/v1/curated?page=1&per_page=80";
   const response = await fetch(baseURL, {
     method: "GET",
     headers: {
@@ -19,27 +19,39 @@ let getImage = async () => {
 function renderDataImage(images) {
   const listImagesHTML = images
     .map((image) => {
-      return `<img src="${image.src.medium}">`;
+      return `<a href="${image.url}"><img src="${image.src.medium}"></a> `;
     })
     .join("");
   document.getElementById("getImg").innerHTML = listImagesHTML;
 }
+
 getImage();
-let step = 80;
-// const searchFunction
-// = async () => {
-//   const baseSearchURL = `https://api.pexels.com/v1/search?query=bikini&per_page=${step}`;
-//   const response = await fetch(baseSearchURL, {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//       Authorization: API_KEY,
-//     },
-//   });
-//   const data = await response.json();
-//   console.log(data);
-//   let images = data.photos;
-//   console.log(images);
-//   renderDataImage(images);
-// };
-// searchFunction();
+
+function onclickinsearch() {
+  console.log("ASAS");
+  document.getElementById("search-more-bottom").innerHTML = `
+  <div class="nav-search-bottom">
+  <div class="motherbox1">
+  <button class="btn btn-success" type="submit" id="button-topic">City</button>
+  <button class="btn btn-success" type="submit" id="button-topic">Holi</button>
+  <button class="btn btn-success" type="submit" id="button-topic">Pretty</button>
+  </div>
+  <div class="motherbox2">
+  <button class="btn btn-success" type="submit" id="button-topic">Natural</button>
+  <button class="btn btn-success" type="submit" id="button-topic">Plant</button>
+  <button class="btn btn-success" type="submit" id="button-topic">People</button>
+  </div>
+  <div class="motherbox3">
+  <button class="btn btn-success" type="submit" id="button-topic">Animal</button>
+  <button class="btn btn-success" type="submit" id="button-topic">Special</button>
+  <button class="btn btn-success" type="submit" id="button-topic">Miracle</button>
+  </div>
+  <div class="motherbox4">
+  <button class="btn btn-success" type="submit" id="button-topic">Ocean</button>
+  <button class="btn btn-success" type="submit" id="button-topic">House</button>
+  <button class="btn btn-success" type="submit" id="button-topic">Environment</button>
+  </div>
+  </div>
+   `;
+  document.getElementById("search-more-bottom").focus;
+}
